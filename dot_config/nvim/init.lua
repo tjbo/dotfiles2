da5921c06567d0a34f760942ae01ef8e9cf229ce
tjbo <tom@prototypable.ca>
2025-03-28 10:55:21 -0300

    updates

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -238,6 +238,7 @@ vim.opt.rtp:prepend(lazypath)
 
 -- NOTE: Here is where you install your plugins.
 require("lazy").setup({
+	{ "farmergreg/vim-lastplace" },
 	{
 		"folke/noice.nvim",
 		event = "VeryLazy",
@@ -559,53 +560,30 @@ require("lazy").setup({
 			},
 		},
 	},
+	{ -- If encountering errors, see telescope-fzf-native README for installation instructions
+		"nvim-telescope/telescope-fzf-native.nvim",
 
+		-- `build` is used to run some command when the plugin is installed/updated.
+		-- This is only run then, not every time Neovim starts up.
+		build = "make",
+
+		-- `cond` is a condition used to determine whether this plugin should be
+		-- installed and loaded.
+		cond = function()
+			return vim.fn.executable("make") == 1
+		end,
+	},
+	{ "nvim-telescope/telescope-ui-select.nvim" },
 	{
 		"nvim-telescope/telescope.nvim",
 		event = "VimEnter",
 		branch = "0.1.x",
 		dependencies = {
 			"nvim-lua/plenary.nvim",
-			{ -- If encountering errors, see telescope-fzf-native README for installation instructions
-				"nvim-telescope/telescope-fzf-native.nvim",
-
-				-- `build` is used to run some command when the plugin is installed/updated.
-				-- This is only run then, not every time Neovim starts up.
-				build = "make",
-
-				-- `cond` is a condition used to determine whether this plugin should be
-				-- installed and loaded.
-				cond = function()
-					return vim.fn.executable("make") == 1
-				end,
-			},
-			{ "nvim-telescope/telescope-ui-select.nvim" },
-
 			-- Useful for getting pretty icons, but requires a Nerd Font.
-			{ "nvim-tree/nvim-web-devicons", enabled = vim.g.have_nerd_font },
+			-- { "nvim-tree/nvim-web-devicons", enabled = vim.g.have_nerd_font },
 		},
 		config = function()
-			-- Telescope is a fuzzy finder that comes with a lot of different things that
-			-- it can fuzzy find! It's more than just a "file finder", it can search
-			-- many different aspects of Neovim, your workspace, LSP, and more!
-			--
-			-- The easiest way to use Telescope, is to start by doing something like:
-			--  :Telescope help_tags
-			--
-			-- After running this command, a window will open up and you're able to
-			-- type in the prompt window. You'll see a list of `help_tags` options and
-			-- a corresponding preview of the help.
-			--
-			-- Two important keymaps to use while in Telescope are:
-			--  - Insert mode: <c-/>
-			--  - Normal mode: ?
-			--
-			-- This opens a window that shows you all of the keymaps for the current
-			-- Telescope picker. This is really useful to discover what Telescope can
-			-- do as well as how to actually do it!
-
-			-- [[ Configure Telescope ]]
-			-- See `:help telescope` and `:help telescope.setup()`
 			local actions = require("telescope.actions")
 
 			local ta = {
@@ -635,24 +613,33 @@ require("lazy").setup({
 			}
 
 			require("telescope").setup({
-				mappings = {
-					i = ta,
-					n = ta,
+				defaults = {
+					mappings = {
+						i = ta,
+						n = ta,
+					},
+					path_display = { "smart" },
+					wrap_results = true,
+					layout_config = {
+						horizontal = { width = 0.95, height = 0.99 },
+						vertical = { width = 0.98, height = 0.98 },
+					},
+					layout_strategy = "vertical",
 				},
-				wrap_results = true,
-				layout_config = {
-					horizontal = { width = 0.95, height = 0.99 },
-					vertical = { width = 0.98, height = 0.98 },
+				pickers = {
+					diagnostics = {
+						previewer = true,
+					},
+					oldfiles = {
+						previewer = false,
+					},
 				},
-				layout_strategy = "vertical",
 				extensions = {
 					["ui-select"] = {
 						require("telescope.themes").get_dropdown(),
 					},
 				},
 			})
-
-			-- Enable Telescope extensions if they are installed
 			pcall(require("telescope").load_extension, "fzf")
 			pcall(require("telescope").load_extension, "ui-select")
 		end,
@@ -1211,17 +1198,30 @@ require("lazy").setup({
 		-- [[ Configure Treesitter ]] See `:help nvim-treesitter`
 		opts = {
 			ensure_installed = {
+				"astro",
 				"bash",
 				"c",
-				"diff",
+				"css",
 				"html",
+				"http",
+				"javascript",
+				"jsdoc",
+				"json",
+				"json5",
 				"lua",
-				"luadoc",
 				"markdown",
 				"markdown_inline",
-				"query",
+				"ocaml",
+				"ocaml_interface",
+				"python",
+				"regex",
+				"ruby",
+				"rust",
+				"toml",
+				"typescript",
 				"vim",
 				"vimdoc",
+				"yaml",
 			},
 			-- Autoinstall languages that are not installed
 			auto_install = true,
--- run_once_install-packages.sh
+++ run_once_install-packages.sh
@@ -4,6 +4,7 @@ brew install zsh
 brew install zsh-syntax-highlighting
 brew install zsh-vi-mode
 brew install zsh-autosuggestions
+brew install nvim
 brew install fzf
 brew install pure
 brew install yarn
@@ -12,6 +13,8 @@ brew install node
 brew install --cask spotify
 brew install httpie
 brew install --cask font-hack
+brew install --cask kitty 
 brew install neofetch
 brew install lazygit
+brew install ripgrep
 brew install karabiner-elements
