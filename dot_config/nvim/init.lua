94829b1980cfdbf823b64544478edf5fbade0017
tjbo <tom@prototypable.ca>
2025-04-08 10:23:21 -0300

    update lazy

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -197,22 +197,6 @@ require("lazy").setup({
 	{ import = "custom.plugins" },
 }, {
 	ui = {
-		-- If you are using a Nerd Font: set icons to an empty table which will use the
-		-- default lazy.nvim defined Nerd Font icons, otherwise define a unicode icons table
-		icons = vim.g.have_nerd_font and {} or {
-			cmd = "⌘",
-			config = "🛠",
-			event = "📅",
-			ft = "📂",
-			init = "⚙",
-			keys = "🗝",
-			plugin = "🔌",
-			runtime = "💻",
-			require = "🌙",
-			source = "📄",
-			start = "🚀",
-			task = "📌",
-			lazy = "💤 ",
-		},
+		icons = {},
 	},
 })
