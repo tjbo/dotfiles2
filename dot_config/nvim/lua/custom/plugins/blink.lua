7f21e9f74ee06e486cb3e3043d61c17ae8e3eeca
tjbo <tom@prototypable.ca>
2026-06-27 12:12:29 -0300

    configure blink

---
+++ dot_config/nvim/lua/custom/plugins/blink.lua
@@ -0,0 +1,36 @@
+return {
+	"saghen/blink.cmp",
+	dependencies = { "rafamadriz/friendly-snippets" },
+	version = "1.*",
+	opts = {
+		keymap = {
+			preset = "none",
+			["<CR>"] = { "select_and_accept", "fallback" },
+			["<C-j>"] = { "select_next", "fallback" },
+			["<C-k>"] = { "select_prev", "fallback" },
+		},
+		appearance = {
+			nerd_font_variant = "mono",
+		},
+		-- (Default) Only show the documentation popup when manually triggered
+		completion = {
+
+			accept = { auto_brackets = { enabled = true } },
+			documentation = { auto_show = false },
+		},
+
+		-- Default list of enabled providers defined so that you can extend it
+		-- elsewhere in your config, without redefining it, due to `opts_extend`
+		sources = {
+			default = { "lsp", "path", "snippets", "buffer" },
+		},
+
+		-- (Default) Rust fuzzy matcher for typo resistance and significantly better performance
+		-- You may use a lua implementation instead by using `implementation = "lua"` or fallback to the lua implementation,
+		-- when the Rust fuzzy matcher is not available, by using `implementation = "prefer_rust"`
+		--
+		-- See the fuzzy documentation for more information
+		fuzzy = { implementation = "prefer_rust_with_warning" },
+	},
+	opts_extend = { "sources.default" },
+}
